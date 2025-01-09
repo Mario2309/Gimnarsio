6e75dfd4c99d77b60115cf134749813b1390761f
Mario2309 <151546364+Mario2309@users.noreply.github.com>
2025-01-09 15:36:10 +0100

    fragmentos x terminar

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,4 +59,7 @@ dependencies {
 
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.3.9")
 
+    val fragment_version = "1.8.3"
+    implementation("androidx.fragment:fragment-ktx:$fragment_version")
+
 }
--- app/src/main/java/es/etg/dam/pmdm13/gym/Inicio.kt
+++ app/src/main/java/es/etg/dam/pmdm13/gym/Inicio.kt
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Build
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.ImageButton
 import android.widget.TextView
@@ -12,52 +13,77 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.add
+import androidx.fragment.app.commit
+import androidx.fragment.app.replace
+import es.etg.dam.pmdm13.gym.databinding.ActivityInicioBinding
 import es.etg.dam.pmdm13.gym.preferencias.LeerPreferencia
+
+
 
 @Suppress("DEPRECATION")
 class Inicio : AppCompatActivity() {
+
+    private lateinit var binding: ActivityInicioBinding
+
+    private var tornoAbierto = true
+
     @SuppressLint("MissingInflatedId", "ShowToast")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        setContentView(R.layout.activity_inicio)
+        binding = ActivityInicioBinding.inflate(layoutInflater)
 
-        val botonDesLogg:ImageButton = findViewById(R.id.imageButtonUnlogg)
+        setContentView(binding.root)
+
         val volverLogin = Intent(this, MainActivity::class.java)
-        val botonPerfil:ImageButton = findViewById(R.id.imageButtonUser)
         val mensaje = "Error!!"
         val duration = Toast.LENGTH_LONG
         val toast = Toast.makeText(this, mensaje,duration)
-        val botonAbrir:Button = findViewById(R.id.btmTorno)
-        val botonEjercicio:Button = findViewById(R.id.btmSelecEjer)
 
         actualizarSaludo()
 
-        botonDesLogg.setOnClickListener {
+        binding.imageButtonUnlogg.setOnClickListener {
             startActivity(volverLogin)
         }
 
-        botonEjercicio.setOnClickListener{
+        binding.btmSelecEjer.setOnClickListener{
             toast.show()
         }
 
-        botonAbrir.setOnClickListener {
-            toast.show()
+        binding.btmTorno.setOnClickListener {
+//            abrirTorno()
         }
 
-        botonPerfil.setOnClickListener {
+        binding.imageButtonUser.setOnClickListener {
             toast.show()
         }
-
 
     }
+
+//    private fun abrirTorno(){
+//
+//        supportFragmentManager.commit {
+//            setReorderingAllowed(true)
+//
+//            if (!tornoAbierto){
+//                replace<codQr>(binding.fragmentContainerViewQr.id)
+//                replace<Calendario>(binding.fragmentContainerViewCalendario.id)
+//            } else {
+//                replace<Calendario>(binding.fragmentContainerViewCalendario.id)
+//                replace<codQr>(binding.fragmentContainerViewQr.id)
+//            }
+//
+//            tornoAbierto = !tornoAbierto
+//        }
+//    }
 
     private fun actualizarSaludo() {
         val leerPreferencia = LeerPreferencia(this)
         val nombre = leerPreferencia.leer()
 
         if (nombre != null) {
-            val textoNombre = findViewById<TextView>(R.id.textViewNomUsu).setText((nombre))
+            val textoNombre = binding.textViewNomUsu.setText((nombre))
         }
     }
 
